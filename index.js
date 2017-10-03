2d367fdb504f10f8cf34b029dc913bf3a14afc22
tahseen447 <tahseenfatima1986@gmail.com>
2017-10-03 18:48:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function takeANumber(katzDeliLine, newPersonsName)
   katzDeliLine.push(newPersonsName)
   return "Welcome, " + newPersonsName + ". You are number "+ katzDeliLine.length + " in line."
 }
+
+function nowServing(array)
+{
+  if(array >0)
+  array.pop()
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
