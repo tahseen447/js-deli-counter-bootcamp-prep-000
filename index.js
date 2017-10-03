a0c57b616708de5bc9f5abef85cc45a550b299f8
tahseen447 <tahseenfatima1986@gmail.com>
2017-10-03 18:44:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli=[]
 function takeANumber(katzDeliLine, newPersonsName)
 {
   katzDeliLine.push(newPersonsName)
-  return "Welcome, " +newPersonsName+". You are number "+ katzDeliLine.length+" in Line."
+  return "Welcome, " +Ada + newPersonsName + ". You are number "+ katzDeliLine.length + " in line."
 }
