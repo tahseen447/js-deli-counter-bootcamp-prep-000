9b1482364025389a271a9cc8e853caeb27b9a3a1
tahseen447 <tahseenfatima1986@gmail.com>
2017-10-03 19:07:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(line)
 {
   if(line.length ===0)
   {
-    return "The line is currently empty"
+    return "The line is currently empty."
   }
   else {
     var str="The line is currently: "
